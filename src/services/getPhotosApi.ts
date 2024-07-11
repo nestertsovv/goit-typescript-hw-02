9848120f849d3c1edc2cac0d50bafdb27b7cf29c
nestertsovv <n.nestertsovv@gmail.com>
2024-07-11 18:38:33 +0300

    updates

--- src/services/getPhotosApi.ts
+++ src/services/getPhotosApi.ts
@@ -2,7 +2,14 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
-export const getPhotos = (query: string, page: number): Promise<any> => {
+type Obj = {
+  data: {
+    results: [];
+    total: number;
+  };
+};
+
+export const getPhotos = (query: string, page: number): Promise<Obj> => {
   const PARAMS = {
     params: {
       client_id: "VnvExAY_-4rz-yTCISF1Dws5SAGUc_IeqsGo_imr8zU",
